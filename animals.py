dba5db8093765540a449532a776533b21d56cf01
Yogev Heskia <yogev911@gmail.com>
2019-10-01 00:59:31 +0300

    added project requirements added images download functionality

--- animals.py
+++ animals.py
@@ -1,8 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import wikipedia
 import traceback
 import conf
+import os
+from multiprocessing import Pool
 
 
 def preprocess_data(raw_text):
@@ -38,8 +41,22 @@ def get_tables():
 
 def parse_tables():
     tables = get_tables()
+    body_tag = ''
     for table in tables:
-        parse_table(table)
+        body_tag += "<br>"
+        body_tag += parse_table(table)
+    html_data = conf.HTML_TEMPLATE(body_tag)
+    with open("index.html", 'w') as f:
+        f.write(html_data)
+
+
+def get_image_from_page(page_name):
+    page = wikipedia.page(page_name)
+    r = requests.get(page.images[0], stream=True)
+    if r.status_code == 200:
+        with open(conf.IMAGES_PATH + page.images[0].split('/')[-1], 'wb') as f:
+            for chunk in r.iter_content(1024):
+                f.write(chunk)
 
 
 def parse_table(table):
@@ -51,18 +68,48 @@ def parse_table(table):
     rows = table.find_all('tr')
     if not rows:
         raise ValueError("No rows for table")
+    pages = []
+    table_tag = "<table>"
     tbl_headers = get_tbl_headers(rows)
+    table_tag += "<tr>"
+    for header in tbl_headers.keys():
+        table_tag += conf.ADD_TH_TAG(header)
+    table_tag += "</tr>"
     for row in rows:
-        line = ''
         cols = row.find_all('td')
         if not cols:
             continue
+        for page_name in cols[0].find_all('a'):
+            if not page_name:
+                continue
+            pages.append(page_name.text)
+        table_tag += '<tr>'
         for header, col in tbl_headers.items():
             try:
-                line += f"{header} : {cols[col].text} \t"
+                table_tag += f"<td>{preprocess_data(f'{header} : {cols[col].text}')} \t</td>"
             except IndexError:
                 pass
-        print(preprocess_data(line))
+        table_tag += '</tr>'
+    table_tag += '</table>'
+    if conf.DOWNLOAD_IMAGES:
+        download_images(pages)
+    return table_tag
+
+
+def download_images(pages):
+    """
+    Download bulk of images via Pool
+    :param pages: list [str]
+    :return: None
+    """
+    try:
+        pool = Pool(conf.MAX_PROCESS)
+        pool.map_async(get_image_from_page, pages)
+        pool.close()
+        pool.join()
+    except:
+        pool.close()
+        pool.join()
 
 
 def get_tbl_headers(rows):
@@ -81,6 +128,8 @@ def get_tbl_headers(rows):
 
 if __name__ == '__main__':
     try:
+        if not os.path.exists(conf.IMAGES_PATH):
+            os.mkdir(conf.IMAGES_PATH)
         parse_tables()
     except requests.exceptions.MissingSchema as e:
         print(f"URL not valid, {e.__str__()}")
--- conf.py
+++ conf.py
@@ -1,3 +1,8 @@
+import os
+
+DOWNLOAD_IMAGES = True
+MAX_PROCESS = os.cpu_count()
+IMAGES_PATH = "tmp/"
 PAGE_URL = "https://en.wikipedia.org/wiki/List_of_animal_names"
 
 TABLE_HEADER_COLS = ["Animal",
@@ -8,3 +13,18 @@ TABLE_HEADER_COLS = ["Animal",
                      "Collateral adjective"]
 
 TABLE_CLASS_NAME = "wikitable"
+
+ADD_TH_TAG = lambda name: f"<tr>{name}</tr>"
+
+HTML_TEMPLATE = lambda body: f"""
+<!doctype html>
+
+<html lang="en">
+<head>
+</head>
+
+<body>
+  {body}
+</body>
+</html>
+"""
